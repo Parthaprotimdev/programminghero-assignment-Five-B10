9b7c6d1407878a374c4dc8df7de200ba3dbb2ff5
Parthaprotimdev <parthaprotimdev@gmail.com>
2024-09-25 11:59:38 +0600

    modify final

--- home.js
+++ home.js
@@ -1,14 +1,18 @@
 // buttons toggle
+
 const donationButton = document.getElementById('donation-btn');
 const historyButton = document.getElementById('history-btn');
 
 // buttons bg toggle
+
 historyButton.addEventListener('click', function () {
   historyButton.classList.add('bg-[#B4F461]');
   donationButton.classList.remove('bg-[#B4F461]');
   historyButton.classList.remove('bg-white', 'btn-border');
   donationButton.classList.add('bg-white', 'btn-border');
+
   // section toggle
+
   document.getElementById('history-section').classList.remove('hidden');
   document.getElementById('donation-section').classList.add('hidden');
 });
@@ -18,12 +22,15 @@ donationButton.addEventListener('click', function () {
   historyButton.classList.remove('bg-[#B4F461]');
   donationButton.classList.remove('bg-white', 'btn-border');
   historyButton.classList.add('bg-white', 'btn-border');
+
   // section toggle
+
   document.getElementById('history-section').classList.add('hidden');
   document.getElementById('donation-section').classList.remove('hidden');
 });
 
 // functions
+
 function getInnerByText(id) {
   return document.getElementById(id).innerText;
 }
@@ -33,19 +40,24 @@ function setInnerById(id, value) {
 }
 
 // donate button functionality Noakhali
+
 const donateButtonNoahali = document.getElementById('donate-btn-noakhali');
 
 donateButtonNoahali.addEventListener('click', function () {
+
   // account balance
+
   const accountBalance = parseFloat(getInnerByText('account-balance'));
 
   // Noakhali balance
+
   const noakhaliBalance = parseFloat(getInnerByText('noakhali-balance'));
 
   const inputNoakhali = document.getElementById('donate-input-noakhali').value;
   const inputNoakhaliNumber = parseFloat(inputNoakhali);
 
   // Input Validation
+
   if (
     inputNoakhali === '' ||
     isNaN(inputNoakhaliNumber) ||
@@ -57,14 +69,18 @@ donateButtonNoahali.addEventListener('click', function () {
   }
 
   // Update account balance
+
   const NewNoakhaliBalance = noakhaliBalance + inputNoakhaliNumber;
   setInnerById('noakhali-balance', NewNoakhaliBalance);
   const newAccountBalance = accountBalance - inputNoakhaliNumber;
   setInnerById('account-balance', newAccountBalance);
 
   // Update history
+
   const noakhaliTitle = document.getElementById('noakhali-title').innerText;
+
   // time
+
   const donateTime = new Date().toLocaleString();
   const historySection = document.getElementById('history-section');
   const newhistoryCardContainer = document.createElement('div');
@@ -86,9 +102,11 @@ donateButtonNoahali.addEventListener('click', function () {
   );
 
   // Clear input
+
   document.getElementById('donate-input-noakhali').value = '';
 
   // Alert modal
+
   document.getElementById('success-modal').classList.add('modal-open');
   document
     .getElementById('close-modal-btn')
@@ -98,20 +116,26 @@ donateButtonNoahali.addEventListener('click', function () {
 });
 
 // donate button functionality Fenni
+
 const donateButtonFeni = document.getElementById('donate-btn-feni');
 
 donateButtonFeni.addEventListener('click', function () {
+
   // account balance
+
   const accountBalance = parseFloat(getInnerByText('account-balance'));
 
   // Noakhali balance
+
   const feniBalance = parseFloat(getInnerByText('feni-balance'));
 
   // Input value
+
   const inputFeni = document.getElementById('donate-input-feni').value;
   const inputFeniNumber = parseFloat(inputFeni);
 
   // Input Validation
+
   if (
     inputFeni === '' ||
     isNaN(inputFeniNumber) ||
@@ -123,14 +147,18 @@ donateButtonFeni.addEventListener('click', function () {
   }
 
   // Update account balance
+
   const NewFeniBalance = feniBalance + inputFeniNumber;
   setInnerById('feni-balance', NewFeniBalance);
   const newAccountBalance = accountBalance - inputFeniNumber;
   setInnerById('account-balance', newAccountBalance);
 
   // Update history
+
   const feniTitle = document.getElementById('feni-title').innerText;
-  // time
+
+  //  time
+
   const donateTime = new Date().toLocaleString();
   const historySection = document.getElementById('history-section');
   const newHistoryCardContainer = document.createElement('div');
